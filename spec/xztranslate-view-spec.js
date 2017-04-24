7b38fb86321fc8a5d4eb5540f5464ad7586cac44
igorvr <igvladr@gmail.com>
2017-04-24 09:27:35 +0300

    Add files via upload

---
+++ spec/xztranslate-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import XztranslateView from '../lib/xztranslate-view';
+
+describe('XztranslateView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
